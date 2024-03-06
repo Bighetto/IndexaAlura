213babc7585b2cfe801da4f939bf9997dca0febe
Arthur Bighetto <arthurbighetto36@gmail.com>
2024-03-06 11:18:44 -0300

    adicionado eventos no html conforme funcoes definidas no componenete
    no button onde esta o (click) existem varias funcoes, como (mouseover) para executar a funcao caso passar por cima do botao por exemplo. pode ser explorado diversas funcoes.

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -28,4 +28,12 @@ export class FormularioContatoComponent {
         observacoes: new FormControl('')
       });
     }
+
+    salvarContato(){
+      console.log(this.contatoForm.value)
+    }
+
+    cancelar(){
+      console.log('Operacao cancelada')
+    }
 }

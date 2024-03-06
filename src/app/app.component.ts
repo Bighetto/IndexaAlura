1b49f31ee84f65599bc4a89edb95eec75783ddc0
Arthur Bighetto <arthurbighetto36@gmail.com>
2024-03-06 20:50:39 -0300

    adicionado rotas com configuracoes no app.routes.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
 import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
 import { CommonModule } from '@angular/common';
+import {RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
+    RouterOutlet,
     FormularioContatoComponent,
     ListaContatosComponent
   ],
